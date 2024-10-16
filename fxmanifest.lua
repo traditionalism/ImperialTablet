8d03ea151984ea81b73aa8bf60e58ee5b34047b7
Lucas <genevasterm@protonmail.ch>
2024-10-16 13:42:32 -0500

    fix: include ImperialLibrary server

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,6 +8,7 @@ file 'Config.ini'
 ui_page 'nui/index.html'
 
 client_script '*.net.dll'
+server_script '*.net.dll'
 
 author 'ImperialSolutions'
 description 'In-game NUI integration for Imperial CAD.'
